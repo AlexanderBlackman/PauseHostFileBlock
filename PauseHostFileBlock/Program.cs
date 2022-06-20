cb4058ff15744795512869e219fc9fa0387cf7ef
AlexanderBlackman <51733876+AlexanderBlackman@users.noreply.github.com>
2022-06-20 16:43:45 +1000

    Add project files.

---
+++ PauseHostFileBlock/Program.cs
@@ -0,0 +1,20 @@
+﻿//Goal: Temporarily renames hostfile if run as admin. Is reset on app closure  or end of specified time.
+
+string hostFileLocation = Environment.SystemDirectory + @"\drivers\etc\hosts";
+string disabledFileLocation = Environment.SystemDirectory + @"\drivers\etc\hosts_Disabled";
+
+try
+{
+    System.IO.FileInfo hostFile = new System.IO.FileInfo(hostFileLocation);
+    if (hostFile.Exists)
+    {
+        hostFile.MoveTo(disabledFileLocation);
+    }
+}
+catch (UnauthorizedAccessException)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("\nSorry, you need admin privs to modify the host file");
+    Console.ReadLine();
+    Console.ResetColor();
+}
